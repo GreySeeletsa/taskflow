8230dc3af2ce28084372dce1f95093b7c4bfa4ca
GreySeeletsa <seeletsagrey@gmail.com>
2024-07-09 18:13:44 +0200

    created redirection of routes when users login

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,27 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware((auth: any, req) => {
+  // Checking if the user is authenticated and the current route is public
+  if (auth.userId && isPublicRoute(req)) {
+    let path = "/select-org";
+    // If the user has an organization ID, redirect them to their organization page
+    if (auth.orgId) {
+      `path = /organization/${auth.orgId}`;
+    }
+    const orgSelection = new URL(path, req.url);
+    // Redirect to the organization selection page
+    return NextResponse.redirect(orgSelection);
+}
+/*if (!auth.userId && !auth.isPublicRoute) {
+    return auth().redirectToSignIn({ returnBackURL: req.url });
+}
+if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+return auth().redirectToSelectOrg({ returnBackURL: req.url }); }*/
+
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
